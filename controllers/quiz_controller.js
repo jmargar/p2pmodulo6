0cd1ea8d5bb38f456381c36195140610bb1cd7bd
jmargar <josmarting@hotmail.com>
2015-07-01 16:46:59 +0100

    P2P busquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,11 +14,19 @@ exports.load = function(req,res,next,quizId){
 
 // GET /quizes
 exports.index = function(req,res){
-	models.Quiz.findAll().then(
-		function(quizes){
-			res.render('quizes/index',{quizes: quizes});
-		}
-	).catch(function(error){next(error);})
+	if(req.query.search){
+		var buscar ='%' + (req.query.search || '').replace(" ","%") + '%';
+		models.Quiz.findAll({where:["pregunta like ?", buscar], order: 'pregunta ASC'}).then(
+			function(quizes){
+				res.render('quizes/index',{quizes: quizes});
+		}).catch(function(error){next(error);});
+	} else {
+		models.Quiz.findAll().then(
+			function(quizes){
+				res.render('quizes/index',{quizes: quizes});
+			}
+		).catch(function(error){next(error);})
+	}
 };
 
 // GET /quizes/:id

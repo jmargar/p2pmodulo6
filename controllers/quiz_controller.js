8731c39177ce77eda0ef453faa88fe0fdc149483
jmargar <josmarting@hotmail.com>
2015-07-03 19:52:13 +0100

    Crear pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,7 +19,8 @@ exports.index = function(req,res){
 		models.Quiz.findAll({where:["pregunta like ?", buscar], order: 'pregunta ASC'}).then(
 			function(quizes){
 				res.render('quizes/index',{quizes: quizes});
-		}).catch(function(error){next(error);});
+			}
+		).catch(function(error){next(error);});
 	} else {
 		models.Quiz.findAll().then(
 			function(quizes){
@@ -41,4 +42,22 @@ exports.answer = function(req,res){
 		resultado = 'Correcto';
 	}
 	res.render('quizes/answer', {quiz:req.quiz, respuesta: resultado});
+};
+
+// GET /quizes/new
+exports.new = function(req,res){
+	var quiz = models.Quiz.build(  //crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+	res.render('quizes/new', {quiz: quiz});
+};
+
+//POST /quizes/create
+exports.create = function(req,res){
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	//guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	}) // Redirección HTTP (URL relativo) lista de preguntas
 };

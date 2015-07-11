39038f1d5e599d3810a7a26a1e3be65005b588cf
jmargar <josmarting@hotmail.com>
2015-07-11 18:04:29 +0100

    Crear comentario

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,9 +1,11 @@
 var models = require('../models/models.js');
 
-// Autoload - factoriza el código si ruta incluye: quizId
-exports.load = function(req,res,next,quizId){
-	models.Quiz.find(quizId).then(
-		function(quiz){
+// Autoload :id
+exports.load = function(req, res, next, quizId){
+	models.Quiz.find({
+				where: {id: Number(quizId)},
+				include: [{model: models.Comment}]
+			}).then(function(quiz){
 			if(quiz){
 				req.quiz = quiz;
 				next();
